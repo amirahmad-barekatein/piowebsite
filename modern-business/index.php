47204f9695bc2cc5e9034006a5fb91edf48c3c1d
Amirahmad <amirahmad.barekatein@gmail.com>
2014-10-26 20:30:41 +0330

    updated

---
+++ modern-business/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include('head.php'); ?>
+	<body>
+		<?php include('header.php'); ?>
+		<!-- Specific for every pages -->
+		<?php include('header_carousel.php'); ?>
+		<!-- End of specification -->
+
+		<!-- Page Content -->
+    	<div class="container">
+    		<!-- Specific for every pages -->
+    		<!-- Marketing Icons Section -->
+    		<div class="row">
+    		    <div class="col-lg-12">
+    		        <h1 class="page-header">
+    		            <strong>
+    		                Parto Imen Qeshm
+    		            </strong>
+    		        </h1>
+    		    </div>
+    		    <div class="col-md-4">
+    		        <div class="panel panel-default text-center">
+    		            <div class="panel-heading">
+    		                <h4 style="color: #ffc500"><span class="glyphicon glyphicon-check"></span> Test</h4>
+    		            </div>
+    		            <div class="panel-body">
+    		                <div class="list-group">
+    		                    <a href="#" class="list-group-item">
+    		                        <span class="glyphicon glyphicon-camera"></span> 
+    		                        Industrial 
+    		                    </a>
+    		                    <a href="#" class="list-group-item">
+    		                        <span class="glyphicon glyphicon-file"></span> 
+    		                        Non Industrial 
+    		                    </a>
+    		                </div>
+    		                <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-align-justify"></span> Learn More</a>
+    		            </div>
+    		        </div>
+    		    </div>
+    		    <div class="col-md-4">
+    		        <div class="panel panel-default text-center" >
+    		            <div class="panel-heading">
+    		                <h4 style="color: #ffc500"><span class="glyphicon glyphicon-pushpin"></span>  Treatment</h4>
+    		            </div>
+    		            <div class="panel-body">
+    		                <div class="list-group">
+    		                    <a href="#" class="list-group-item">
+    		                        <span class="glyphicon glyphicon-camera"></span> 
+    		                        Chemical 
+    		                    </a>
+    		                    <a href="#" class="list-group-item">
+    		                        <span class="glyphicon glyphicon-file"></span> 
+    		                        Physical 
+    		                    </a>
+    		                </div>
+    		                <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-align-justify"></span> Learn More</a>
+    		            </div>
+    		        </div>
+    		    </div>
+    		    <div class="col-md-4">
+    		        <div class="panel panel-default text-center">
+    		            <div class="panel-heading">
+    		                <h4 style="color: #ffc500"><span class="glyphicon glyphicon-shopping-cart"></span> Trading</h4>
+    		            </div>
+    		            <div class="panel-body">
+    		                <div class="list-group">
+    		                    <a href="#" class="list-group-item">
+    		                        <span class="glyphicon glyphicon-camera"></span> 
+    		                        International 
+    		                    </a>
+    		                    <a href="#" class="list-group-item">
+    		                        <span class="glyphicon glyphicon-file"></span> 
+    		                        Domestic
+    		                    </a>
+    		                </div>
+    		                <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-align-justify"></span> Learn More</a>
+    		            </div>
+    		        </div>
+    		    </div>
+    		</div>
+    		<!-- Features Section -->
+    		<div class="row">
+    		    <div class="col-lg-12">
+    		        <h2 class="page-header">Business Features</h2>
+    		    </div>
+    		    <div class="col-md-6">
+    		        <p>PIOC proudly presents : </p>
+    		        <ul>
+    		            <li><strong>Item 1</strong>
+    		            </li>
+    		            <li>Item 2</li>
+    		            <li>Item 3 </li>
+    		        </ul>
+    		        <p>Features Description</p>
+    		    </div>
+    		    <div class="col-md-6">
+    		        <img class="img-responsive" src="oil.jpg" alt="">
+    		    </div>
+    		</div>
+    		<!-- End of specfication-->
+    		<?php include('footer.php'); ?>
+    	</div>
+	</body>
+	<?php include('script_include.php'); ?>
+</html>
