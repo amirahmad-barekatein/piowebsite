c3e393cc229a92a24dd09f6fc20df174f7929975
Amirahmad <amirahmad.barekatein@gmail.com>
2014-10-26 13:11:38 +0330

    first template

---
+++ modern-business/test.php
@@ -0,0 +1,195 @@
+<?php include('header.php'); ?>
+  <!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/002040/FFC500&text=PIOC');"></div>
+                <div class="carousel-caption">
+                    <h2></h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/002040/FFC500&text=Oil Test');"></div>
+                <div class="carousel-caption">
+                    <h2></h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/002040/FFC500&text=Oil Purification');"></div>
+                <div class="carousel-caption">
+                    <h2></h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    <strong>
+                        Parto Imen Qeshm
+                    </strong>
+                </h1>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Oil Testing</h4>
+                    </div>
+                    <div class="panel-body">
+                        <!--<p>Testing</p>-->
+                        <!-- <ul class="list-group">
+                            <li class="list-group-item">Test 1</li>
+                            <li class="list-group-item">Test 2</li>
+                            <li class="list-group-item">Test 3 </li>
+                        </ul> -->
+
+
+                        <div class="list-group">
+                            <a href="#" class="list-group-item">
+                                <span class="glyphicon glyphicon-camera"></span> Pictures <span class="badge">New</span>
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <span class="glyphicon glyphicon-file"></span> Documents 
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <span class="glyphicon glyphicon-music"></span> Music 
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <span class="glyphicon glyphicon-film"></span> Videos 
+                            </a>
+                        </div>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i> Oil Purification</h4>
+                    </div>
+                    <div class="panel-body">
+                        <!--<p>Purifating</p>-->
+                        <ul>
+                            <li>Purifating 1</li>
+                            <li>Purifating 2</li>
+                            <li>Purifating 3 </li>
+                        </ul>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i> Easy to Use</h4>
+                    </div>
+                    <div class="panel-body">
+                        <!--<p>Usage</p>-->
+                        <ul>
+                            <li>Usage 1</li>
+                            <li>Usage 2</li>
+                            <li>Usage 3 </li>
+                        </ul>
+                        <a href="#" class="btn btn-default">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <!--<div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Portfolio Heading</h2>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="portfolio-item.html">
+                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+                </a>
+            </div>
+        </div>-->
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Business Features</h2>
+            </div>
+            <div class="col-md-6">
+                <p>PIOC proudly presents : </p>
+                <ul>
+                    <li><strong>Item 1</strong>
+                    </li>
+                    <li>Item 2</li>
+                    <li>Item 3 </li>
+                </ul>
+                <p>Features Description</p>
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="oil.jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>Description</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-default btn-block" href="#">Contact Us</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+<?php include('footer.php'); ?>
