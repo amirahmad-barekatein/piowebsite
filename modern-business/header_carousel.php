01fe81330e89d6cdbcc55e97f84707e9b0f51842
Amirahmad <amirahmad.barekatein@gmail.com>
2014-10-26 20:35:22 +0330

    Updated

---
+++ modern-business/header_carousel.php
@@ -0,0 +1,37 @@
+<!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/FFC500/002040&text=OIL');">
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/FFC500/002040&text=Test');">
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/FFC500/002040&text=Treatment');">
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080/FFC500/002040&text=Trading');">
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
---
+++ modern-business/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include('head.php'); ?>
+	<body>
+		<?php include('header.php'); ?>
+		<!-- Specific for every pages -->
+		<!-- End of specification -->
+
+		<!-- Page Content -->
+    	<div class="container">
+    		<!-- Specific for every pages -->
+
+    		<!-- End of specfication-->
+    		<?php include('footer.php'); ?>
+    	</div>
+	</body>
+	<?php include('script_include.php'); ?>
+</html>
